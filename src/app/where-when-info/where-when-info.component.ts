a71a38fbeecf08aec7e6bd8614943ab24530ee61
Raymundo Jimenez <jacksmanchan@msn.com>
2022-10-04 17:36:31 -0500

    ysh-004 Add where-when-info component

---
+++ src/app/where-when-info/where-when-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-where-when-info',
+  templateUrl: './where-when-info.component.html',
+  styleUrls: ['./where-when-info.component.css']
+})
+export class WhereWhenInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
